22405109e9a9bf27650f033f92ae60c01b1f5527
Rishabh Pahwa <rishabhpahwa@gmail.com>
2023-07-10 04:12:38 -0400

    Add Models and Homepage

--- eAcademyApp/models.py
+++ eAcademyApp/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE)
+    files = models.FileField(upload_to='course_files/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Membership(models.Model):
+    MEMBERSHIP_CHOICES = [
+        ('gold', 'Gold'),
+        ('silver', 'Silver'),
+        ('bronze', 'Bronze'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    membership_type = models.CharField(max_length=10, choices=MEMBERSHIP_CHOICES, default='bronze')
+
+    def __str__(self):
+        return self.user.username
+
+
+class Payment(models.Model):
+    CURRENCY_CHOICES = [
+        ('USD', 'USD'),
+        ('EUR', 'EUR'),
+        ('GBP', 'GBP'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
+    payment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} {self.currency}"
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    attendance = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+    grade = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.user.username

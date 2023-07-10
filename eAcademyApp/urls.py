92f5138df0cbd754a15f5a04d35f209817337f2b
Rishabh Pahwa <rishabhpahwa@gmail.com>
2023-07-10 19:50:59 -0400

    Update homepage + bug fixes

--- eAcademyApp/urls.py
+++ eAcademyApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
+
 ]

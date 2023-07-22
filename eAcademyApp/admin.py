2cc5105d54534aca252f7c12bc9bdb1b97b10c9c
r1shabhpahwa <rishabhpahwa@gmail.com>
2023-07-22 05:36:04 -0400

    Misc fixes

--- eAcademyApp/admin.py
+++ eAcademyApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Course, Membership, Payment, UserProfile, InstructorRequest
+from .models import Course, Membership, Payment, UserProfile, InstructorRequest, Enrollment
 
 admin.site.register(Course)
 admin.site.register(Membership)
 admin.site.register(Payment)
 admin.site.register(UserProfile)
 admin.site.register(InstructorRequest)
+admin.site.register(Enrollment)
